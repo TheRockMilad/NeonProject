1a02a97ee148c9aa85ef6b77687663f19fcf0aa6
TheRockMilad <therock.milad2@outlook.com>
2024-05-19 01:38:42 +0330

    Added debug to listen & Added start on package.json

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const debug = require('debug')('app:main')
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+app.listen(PORT, () => debug(`listening on port ${PORT}`));
