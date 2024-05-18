61f64a62aed2227a97141907e07e5dfabb8ca017
TheRockMilad <therock.milad2@outlook.com>
2024-05-19 01:45:56 +0330

    Added router Auth

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 const debug = require('debug')('app:main')
+const router = require('./src/routes')
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.use('/api',router)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => debug(`listening on port ${PORT}`));
