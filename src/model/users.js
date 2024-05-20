c8ece1fb288ef1b7b50579e1839e8af098a7c18f
TheRockMilad <therock.milad2@outlook.com>
2024-05-20 19:35:34 +0330

    Added User model new file:   src/model/users.js

---
+++ src/model/users.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const timestamp = require("mongoose-timestamp");
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  family: { type: String, required: false },
+  age: { type: Number, required: false },
+  password: { type: String, required: true },
+  isAdmin: { type: Boolean, default: false },
+  isActive: { type: Boolean, default: false },
+  avatarUrl: { type: String, required: false }
+});
+
+mongoose.plugin(timestamp);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
