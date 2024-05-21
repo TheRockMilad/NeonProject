d19fb2d17d63f5eeb01d1655ed4571a6f5c46d80
TheRockMilad <therock.milad2@outlook.com>
2024-05-21 15:07:57 +0330

    Added Auth middleware and cookie

---
+++ src/middleware/auth.js
@@ -0,0 +1,19 @@
+const { User, response } = require("../routes/auth/controller");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { find } = require("lodash");
+
+async function isLoggined(req, res, next) {
+  const token = req.cookies.authToken;
+  if (!token) {
+    response({
+      res,
+      code: 401,
+      message: "access denied",
+    });
+  }
+  const decoded = jwt.verify(token, config.get(jwt));
+  const user = User.findById(decoded._id);
+  req.user = user;
+  next();
+}
