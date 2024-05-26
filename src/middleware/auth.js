e9c617b7ab78a3cac301b2262ae56f17d9a76e18
TheRockMilad <therock.milad2@outlook.com>
2024-05-26 13:40:10 +0330

    Fix bug and added TryCatch

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,26 +5,37 @@ const config = require("config");
 async function isLoggined(req, res, next) {
   const token = req.cookies.authToken;
   if (!token) {
-    response({
+    return response({
       res,
       code: 401,
-      message: "access denied",
+      message: "access denied _ User not logged in ",
     });
   }
-  const decoded = jwt.verify(token, config.get("jwt"));
-  const user = await User.findById(decoded._id);
-  req.user = user;
-  next();
+  try {
+    const decoded = jwt.verify(token, config.get("jwt"));
+    const user = await User.findById(decoded._id);
+    req.user = user;
+    next();
+  } catch (error) {
+    return response({
+      res,
+      code: 400,
+      message: "Invalid token.",
+    });
+  }
 }
 
 async function isAdmin(req, res, next) {
   if (!req.user.isAdmin) {
-    return response({ res, message: "Access denied" });
+    return response({
+      res,
+      code: 403,
+      message: "Access denied. Admin privileges required ",
+    });
   }
   next();
 }
-
 module.exports = {
   isLoggined,
-  isAdmin
+  isAdmin,
 };
