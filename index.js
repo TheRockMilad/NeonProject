0765cd048d19900b5d2b8265fa2686c148906274
TheRockMilad <therock.milad2@outlook.com>
2024-05-26 13:34:14 +0330

    Created middleware 404

--- index.js
+++ index.js
@@ -5,20 +5,23 @@ const dbdebug = require("debug")("db:main");
 const router = require("./src/routes");
 const mongoose = require("mongoose");
 const config = require("config");
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use("/api", router);
 
-mongoose.connect(config.get('database.address'))
-.then(() => dbdebug("connect to database"))
-.catch((err) => dbdebug(`couldn't connect to database (${err})`));
+app.use((req, res) => {
+  res.status(404).send("This API does not exist");
+});
 
-  
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => debug(`listening on port ${PORT}`));
-  
+mongoose
+  .connect(config.get("database.address"))
+  .then(() => dbdebug("connect to database"))
+  .catch((err) => dbdebug(`couldn't connect to database (${err})`));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => debug(`listening on port ${PORT}`));
