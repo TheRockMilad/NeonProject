cc3a4cd036487048154b029e16d3ae3e7cb6bf30
TheRockMilad <therock.milad2@outlook.com>
2024-05-28 02:16:11 +0330

    Created article route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const authRouter = require("./auth");
 const userRouter = require("./user");
 const adminRouter = require("./admin");
+const articleRouter = require("./article");
 const { isLoggined, isAdmin } = require("./../middleware/auth");
 
 router.use("/auth", authRouter);
 router.use("/user", isLoggined, userRouter);
+router.use("/article", isLoggined, articleRouter);
 router.use("/admin", isLoggined, isAdmin, adminRouter);
 
 module.exports = router;
