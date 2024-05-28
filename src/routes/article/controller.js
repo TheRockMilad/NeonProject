fbb911e89082a720e64f0c784f8ee0e0f838cc39
TheRockMilad <therock.milad2@outlook.com>
2024-05-28 03:34:08 +0330

    Completed  "creating articles"

--- src/routes/article/controller.js
+++ src/routes/article/controller.js
@@ -3,7 +3,38 @@ const _ = require("lodash");
 
 module.exports = new (class extends controller {
   async CreateArticle(req, res) {
-    res.send(`create article`);
+    try {
+      const { title, content } = _.pick(req.body, ["title", "content"]);
+      const user = req.user;
+
+      const newArticle = new this.Article({
+        title,
+        content,
+        author: user._id,
+        published : true
+      });
+
+      const savedArticle = await newArticle.save();
+
+      user.articles.push(savedArticle._id);
+      await user.save();
+      this.response({
+        res,
+        code: 201,
+        message: "Article Created",
+        data: {
+          user: user.name,
+          title,
+          content,
+        },
+      });
+    } catch (error) {
+      this.response({
+        res,
+        code: 500,
+        message: "Error",
+      });
+    }
   }
   async showAllArticle(req, res) {
     res.send(`show article`);
