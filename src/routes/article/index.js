580457edd7e0c6dd9ba0a985c55a05f422914851
TheRockMilad <therock.milad2@outlook.com>
2024-05-28 03:34:37 +0330

    change path article to /

--- src/routes/article/index.js
+++ src/routes/article/index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const controller = require("./controller");
 const router = express.Router();
 
-router.post("article", controller.CreateArticle);
-router.get("article", controller.showAllArticle);
-router.put("article", controller.editArticle);
-router.delete("article", controller.deleteArticle);
+router.post("/", controller.CreateArticle);
+router.get("/", controller.showAllArticle);
+router.put("/", controller.editArticle);
+router.delete("/", controller.deleteArticle);
 
 module.exports = router;
