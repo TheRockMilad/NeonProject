7e7476c42a531efde420b9daecb2863b6675b3e7
TheRockMilad <therock.milad2@outlook.com>
2024-05-28 16:07:35 +0330

    Edited validation article

--- src/routes/article/index.js
+++ src/routes/article/index.js
@@ -3,7 +3,12 @@ const controller = require("./controller");
 const validator = require("./validator");
 const router = express.Router();
 
-router.post("/", validator.CreatreArticleValidation, controller.CreateArticle);
+router.post(
+  "/",
+  validator.CreatreArticleValidation(),
+  controller.validate,
+  controller.CreateArticle
+);
 router.get("/", controller.showAllArticle);
 router.put("/", controller.editArticle);
 router.delete("/", controller.deleteArticle);
